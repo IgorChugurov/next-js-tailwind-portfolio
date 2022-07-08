08b24efc0e85291635e6c97d5b4eac0a8a7a921c
IgorChugurov <igorchugurov@gmail.com>
2022-07-08 21:40:22 +0300

    spell checking

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const Homepage = () => {
       logo: "/mebelmag.svg",
       color: "#561824",
 
-      description: `Marketplace for manufacturers and sellers of furniture. Close the dialog
+      description: `Marketplace for manufacturers and sellers of furniture.
         The goal of the project is to create a trading platform where Ukrainian furniture manufacturers will be represented. Creation of a unified catalog with a standard set of characteristics and characteristics for each category of goods.`,
       technologies: ["TypeScript", "JavaScript", "React, Next.js", "CSS, SCSS", "Tailwindcss"],
       links: [{ text: "Site", href: "https://mebel-mag.com.ua/" }],
