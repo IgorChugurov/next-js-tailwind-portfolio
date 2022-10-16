aee56b352abc4889462d8680e756ac0ac258bbe5
IgorChugurov <igorchugurov@gmail.com>
2022-10-16 22:50:09 +0300

    center logo in vertical

--- pages/components/PortfolioSection_old.jsx
+++ pages/components/PortfolioSection_old.jsx
@@ -4,13 +4,13 @@ const PortfolioSection = ({ title, logo, description, technologies, links, color
   return (
     <section>
       <header
-        className="flex justify-between pt-10 pr-10 pl-10 lg:pt-10 lg:pr-10 lg:pl-10"
+        className="flex h-44  justify-between pr-10  pl-10 lg:pr-10 lg:pl-10"
         style={{ backgroundColor: color }}
       >
-        <h2 className=" self-end font-serif text-2xl text-zinc-50 sm:text-3xl lg:text-4xl">
+        <h2 className="mb-2 self-end font-serif text-2xl text-zinc-50 sm:text-3xl lg:text-4xl">
           {title}
         </h2>
-        <div className=" flex h-32 w-32 items-center justify-center bg-white">
+        <div className=" mt-6 flex h-32 w-32 items-center justify-center bg-white">
           <div className="relative h-20 w-20">
             <Image src={logo} alt={title} layout="fill" objectFit="container" />
           </div>
